4f77585f6a6fb0c517ef61d5633b351942219a54
Chandramohan97 <chandramsriv@gmail.com>
2023-02-18 16:18:46 +0530

    feat : testing API.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { Passport } = require("passport");
-
 const app = require("express")();
 require("dotenv").config();
-
 const port = process.env.PORT || 8080;
-console.log(port);
+// console.log(port);
 
-console.log(faker);
+app.use("/", (req, res) => {
+  res.send("API running fine");
+});
+
 app.listen(port, () => {
   console.log(`Server started on Port : ${port}`);
 });
