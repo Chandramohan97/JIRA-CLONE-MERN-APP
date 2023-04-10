be30cf65a393f2bba0725afb023899a053071d96
Chandramohan97 <chandramsriv@gmail.com>
2023-04-10 13:10:02 +0530

    including the Project Creation Route in server.js

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const bodyParser = require('body-parser');
 const {mongoConnect} = require('./config/db')
 const userRoute = require("./routes/userRoute")
 const port = process.env.PORT || 8080;
+const projectRoute = require('./routes/projectRoute')
 const cors = require('cors');
 
 app.use(cors());
 
-
+app.use(bodyParser.json());
 mongoConnect();
 
 app.get("/", (req, res) => {
   res.send("Api working");
 });
 
+app.use('/projectCreation',projectRoute);
 app.use('/user',userRoute)
 
 app.listen(port, () => {
